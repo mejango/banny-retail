bbd454e246ee0cafca8fb64005b95097c4625033
mejango <mejango@protonmail.com>
2025-02-18 00:45:51 -0300

    fmt

--- src/Banny721TokenUriResolver.sol
+++ src/Banny721TokenUriResolver.sol
@@ -450,7 +450,9 @@ contract Banny721TokenUriResolver is
 
         // Add the background if needed.
         if (backgroundId != 0 && shouldIncludeBackgroundOnBannyBody) {
-            contents = string.concat(contents, _svgOf({hook: hook, upc: _productOfTokenId({hook: hook, tokenId: backgroundId}).id}));
+            contents = string.concat(
+                contents, _svgOf({hook: hook, upc: _productOfTokenId({hook: hook, tokenId: backgroundId}).id})
+            );
         }
 
         // Start with the banny body.
